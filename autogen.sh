db9122d7ec78bfe544cf6f5ca6e8d2941f0e104a
Zhiqiang <zma@localhost.localdomain>
2009-11-29 23:40:36 +0800

    Change the programming language to C from OCaml.

--- autogen.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-# Run this file with no arguments to set up your development environment
-
-aclocal -I m4
-automake --add-missing
-autoconf
--- iptables-helper.ml
+++
@@ -1,5 +0,0 @@
-let main () =
-  print_string "Hello world!\n";
-  exit 0;;
-
-main ();;
