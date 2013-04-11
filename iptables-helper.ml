48fbeddda12a4a479727cfd179383cba9ae4df28
Zhiqiang Ma <ericzqma@gmail.com>
2013-04-12 02:00:27 +0800

    read in iptables as strings; rm comments

--- iptables-helper.ml
+++ iptables-helper.ml
@@ -11,11 +11,29 @@ let read_in_rules filename =
         List.rev !lines
 ;;
 
+let rec print_rules = function 
+    | [] -> ()
+    | e::l -> print_string e ; print_string "\n" ; print_rules l
+;;
+
+let rec rm_comments = function
+    | [] -> []
+    | h :: t -> if h.[0] = '#' then rm_comments t else h :: (rm_comments t)
+;;
+
 (* args *)
 if Array.length Sys.argv != 2 then
     raise (Invalid_argument "args is wrong. input the filename only")
+;;
 
-let filename = Sys.argv.(0)
+let filename = Sys.argv.(1);;
 
-let rules = read_in_rules filename
+let rules = read_in_rules filename;;
+
+(* print_rules rules;; *)
+
+let rules2 = rm_comments rules;;
+
+print_rules rules2;;
+
 
