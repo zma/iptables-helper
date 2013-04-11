bbd5ca1deb2e050cffe096e38d6846d1a8a01b62
Zhiqiang Ma <ericzqma@gmail.com>
2013-04-12 01:40:57 +0800

    add some initial trying code

--- iptables-helper.ml
+++ iptables-helper.ml
@@ -1,5 +1,21 @@
-let main () =
-  print_string "Hello world!\n";
-  exit 0;;
 
-main ();;
+let read_in_rules filename =
+    let lines = ref [] in
+    let chan = open_in filename in
+    try
+        while true; do
+            lines := input_line chan :: !lines
+        done; []
+    with End_of_file ->
+        close_in chan;
+        List.rev !lines
+;;
+
+(* args *)
+if Array.length Sys.argv != 2 then
+    raise (Invalid_argument "args is wrong. input the filename only")
+
+let filename = Sys.argv.(0)
+
+let rules = read_in_rules filename
+
