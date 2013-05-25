666fc28252b3f74b0a1e3ea33cb2c6b65cdd4aa3
Zhiqiang Ma <ericzqma@gmail.com>
2013-05-25 16:38:59 +0800

    format code

--- IptablesHelper.ml
+++ IptablesHelper.ml
@@ -1,36 +1,35 @@
 
 let read_in_rules filename =
-    let lines = ref [] in
-    let chan = open_in filename in
+  let lines = ref [] in
+  let chan = open_in filename in
     try
-        while true; do
-            lines := input_line chan :: !lines
-        done; []
+      while true do
+        lines := input_line chan :: !lines
+      done; []
     with End_of_file ->
-        close_in chan;
-        List.rev !lines
+      close_in chan;
+      List.rev !lines
 ;;
 
 let rec print_rules = function 
-    | [] -> ()
-    | e::l -> print_string e ; print_string "\n" ; print_rules l
+    [] -> ()
+  | e::l -> print_string e ; print_string "\n" ; print_rules l
 ;;
 
 let rec rm_comments = function
-    | [] -> []
-    | h :: t -> if h.[0] = '#' then rm_comments t else h :: (rm_comments t)
+    [] -> []
+  | h :: t -> if h.[0] = '#' then rm_comments t else h :: (rm_comments t)
 ;;
 
 let helper file_in =
   let rules = read_in_rules file_in in
-  (* print_rules rules;; *)
   let rules2 = rm_comments rules in
+    (* print_rules rules;; *)
     print_rules rules2
 ;;
 
-(* args *)
+(* start works *)
 match Sys.argv with
     [| _; file_in |] -> helper file_in
   | _ -> raise (Invalid_argument "args is wrong. input the filename only")
-
 
